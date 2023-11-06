594a184c6521fbcbca40dacba164026fd077ebe6
Amy Jung <ajwjung@outlook.com>
2023-11-06 14:21:55 -0800

    chore: Remove unused logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from "./components/App.jsx"
-// import ShopMain from "./components/ShopPages/ShopMain.jsx"
-// import Cart from "./components/Cart/Cart.jsx"
 import './index.css'
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "categories/:category",
-//     element: <ShopMain />,
-//   },
-//   {
-//     path: "cart",
-//     element: <Cart />,
-//   }
-// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
