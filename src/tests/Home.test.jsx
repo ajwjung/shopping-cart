a4d9d907d24616e14bd81475afb070be510cbd95
Amy Jung <ajwjung@outlook.com>
2023-11-06 14:20:32 -0800

    fix: Change App.test to Home.test and update tests

--- src/tests/Home.test.jsx
+++ src/tests/Home.test.jsx
@@ -1,27 +1,13 @@
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
-import App from "../components/App";
+import Home from "../components/Home/Home";
 
-describe("App component (home page)", () => {
-    it("shows a loading screen while fetching for data", async () => {        
-        act(() => {
-            render(
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            );
-        });
-
-        const loadingText = screen.getByText("Loading...");
-
-        expect(loadingText).toBeInTheDocument();
-    })
-
+describe("Home component", () => {
     it("renders nav bar", () => {
         render(
             <BrowserRouter>
-                <App />
+                <Home />
             </BrowserRouter>
         )
 
@@ -34,7 +20,7 @@ describe("App component (home page)", () => {
     it("renders shop overview content", () => {
         render(
             <BrowserRouter>
-                <App />
+                <Home />
             </BrowserRouter>
         )
 
