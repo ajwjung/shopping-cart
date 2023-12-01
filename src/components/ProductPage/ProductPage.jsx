3b48e3fd64a4bb26835b5d1f7639bdf0dd2422a6
Amy Jung <ajwjung@outlook.com>
2023-12-01 14:27:16 -0800

    style: Product pages for small screens

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import ErrorPage from "../ShopPages/ErrorPage";
 import { ShopContext } from "../App";
 import RatingStars from "./RatingStars";
+import Price from "../../scripts/Price";
+import Display from "../../scripts/Display";
+import styles from "./ProductPage.module.css";
 
 function useProductData(productId) {
     const [productData, setProductData] = useState(null);
@@ -46,9 +50,9 @@ function ProductPage() {
     function handleChangeQuantity(e) {
         const inputValue = Number(e.target.value);
 
-        if (e.target.className === "decrement") {
+        if (e.target.className === `${styles.decreaseBtn}`) {
             (quantity - 1 > 0) && setQuantity(quantity - 1);
-        } else if (e.target.className === "increment") {
+        } else if (e.target.className === `${styles.increaseBtn}`) {
             (quantity + 1 <= productData.stock) && setQuantity(quantity + 1);
         } else if (e.target.id === "quantity") {
             // Highest accepted value is the max number in stock
@@ -62,48 +66,95 @@ function ProductPage() {
         }
     }
 
+    function formatCategoryName(category) {
+        if (category === "home-decoration") {
+            return "Home Decor";
+        } else {
+            return Display.capitalizeName(category);
+        }
+    }
+    
     if (error) return <ErrorPage />;
     if (loading) return <p>Loading...</p>;
 
     return (
         <>
             <Navbar />
-            <main className="product" >
-                <img src={productData.thumbnail} alt={productData.title} />
-                <div className="productInfo">
-                    <h1 className="productName">{productData.title}</h1>
-                    <h2 className="productBrand">{productData.brand}</h2>
-                    <p className="productRating">
-                        <RatingStars rating={productData.rating} /> 
-                        {productData.rating} rating
-                    </p>
-                    <p className="productPrice">${productData.price}</p>
-                    <div className="qtyBtns">
-                        <button 
-                            onClick={handleChangeQuantity}
-                            className="decrement" 
-                            type="button"
-                        >-</button>
-                        <label htmlFor="quantity">Quantity</label>
-                        <input 
-                            type="text" 
-                            id="quantity" 
-                            name="quantity" 
-                            value={quantity}
-                            onChange={handleChangeQuantity}
-                            pattern="[0-9]*"
+            <main className={styles.productContent}>
+                <div className={styles.linksWrapper}>
+                    <Link className={styles.link} to="/categories/all">
+                        All
+                    </Link>
+                    <span> / </span>
+                    <Link className={styles.link} to={`/categories/${productData.category === "home-decoration"
+                    ? "home-decor" : productData.category}`}>
+                        {formatCategoryName(productData.category)}
+                    </Link>
+                </div>
+                <div className={styles.productInfo}>
+                    <h1 className={styles.productName}>
+                        {Display.capitalizeName(productData.title)}
+                    </h1>
+                    <h2 className={styles.brand}>
+                        {productData.brand}
+                    </h2>
+                    <div className={styles.imgWrapper}>
+                        <img 
+                            className={styles.productImage}
+                            src={productData.thumbnail} 
+                            alt={productData.title} 
                         />
-                        <button 
-                            onClick={handleChangeQuantity}
-                            className="increment" 
-                            type="button"
-                        >+</button>
+                    </div>
+                    <div className={styles.ratings}>
+                        <RatingStars rating={productData.rating} /> 
+                        <p className={styles.productRating}>
+                            {productData.rating} rating
+                        </p>
+                    </div>
+                    <div className={styles.price}>
+                        {productData.discountPercentage > 0 &&
+                        <p className={`${styles.bold} ${styles.discountPrice}`}>
+                            {`$${Price.calculateDiscountPrice(
+                                productData.discountPercentage, 1, productData.price
+                            )}`}
+                        </p>}
+                        <p className={styles.originalPrice}>
+                            {`$${productData.price.toFixed(2)}`}
+                        </p>
+                    </div>
+                    <div className={styles.qtyWrapper}>
+                        <label 
+                            className={styles.qtyLabel} 
+                            htmlFor="quantity">
+                                Quantity
+                        </label>
+                        <div className={styles.qtyBtns}>
+                            <button
+                                className={styles.decreaseBtn}
+                                onClick={handleChangeQuantity}
+                                type="button"
+                            >-</button>
+                            <input
+                                type="text"
+                                className={styles.quantity}
+                                id="quantity"
+                                name="quantity"
+                                value={quantity}
+                                onChange={handleChangeQuantity}
+                                pattern="[0-9]*"
+                            />
+                            <button
+                                onClick={handleChangeQuantity}
+                                className={styles.increaseBtn}
+                                type="button"
+                            >+</button>
+                        </div>
                     </div>
                     <button 
                         onClick={() => {
                             handleAddToCart(productId, quantity);
                         }}
-                        className="addToCart" 
+                        className={`${styles.addToCartBtn} ${styles.bold}`}
                         type="button"
                     >Add to Cart</button>
                 </div>
