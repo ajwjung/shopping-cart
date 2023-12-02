5b5dac3e5304c201c1dd8e30260212ae60f2cc07
Amy Jung <ajwjung@outlook.com>
2023-12-01 18:05:26 -0800

    style: Home page for small devices

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,40 @@
 import Navbar from "../Navbar/Navbar";
 import ShopCategory from "./ShopCategory";
+import Display from "../../scripts/Display";
 import styles from "./Home.module.css";
 
 function Home() {
+    const imgLinks = Display.homeImages;
+
     return (
         <>
-        <Navbar />
-        <main className={styles.homeContent} >
-            <ShopCategory 
-                categoryName="All" 
-                image="https://images.unsplash.com/photo-1449247709967-d4461a6a6103?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-            />
-            <ShopCategory 
-                categoryName="Lighting" 
-                image="https://images.unsplash.com/photo-1524484485831-a92ffc0de03f?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-            />
-            <ShopCategory 
-                categoryName="Home Decor" 
-                image="https://images.unsplash.com/photo-1524634126442-357e0eac3c14?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-            />
-            <ShopCategory 
-                categoryName="Furniture" 
-                image="https://images.unsplash.com/photo-1519710164239-da123dc03ef4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-            />
-        </main>
+            <Navbar />
+            <div className={styles.homeContent}>
+                <div className={styles.bannerWrapper}>
+                    <img className={styles.banner} src={imgLinks[4]} alt="" />
+                    <p className={styles.adText}>
+                        Shop your dream aesthetic at the right price.
+                    </p>
+                </div>
+                <div className={styles.categories}>
+                    <ShopCategory
+                        categoryName="All"
+                        image={imgLinks[0]}
+                    />
+                    <ShopCategory
+                        categoryName="Lighting"
+                        image={imgLinks[1]}
+                    />
+                    <ShopCategory
+                        categoryName="Home Decor"
+                        image={imgLinks[2]}
+                    />
+                    <ShopCategory
+                        categoryName="Furniture"
+                        image={imgLinks[3]}
+                    />
+                </div>
+            </div>
         </>
     )
 }
--- src/scripts/Display.js
+++ src/scripts/Display.js
@@ -1,11 +1,19 @@
 const Display = (() => {
+    const homeImages = [
+        "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1524484485831-a92ffc0de03f?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1623244307563-f9ade3df13c0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1519710164239-da123dc03ef4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1600210491892-03d54c0aaf87?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"    
+    ]
+
     function capitalizeName(name) {
         return name.split(" ").map(word => {
             return word.charAt(0).toUpperCase() + word.slice(1)
         }).join(" ");
     }
 
-    return { capitalizeName };
+    return { homeImages, capitalizeName };
 })();
 
 export default Display;
