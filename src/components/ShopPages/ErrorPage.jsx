8c0c061671ca2c532eb564835f2cad7f34483cc5
Amy Jung <ajwjung@outlook.com>
2023-12-04 15:08:37 -0800

    fix: Only show error page if error

--- src/components/ShopPages/ErrorPage.jsx
+++ src/components/ShopPages/ErrorPage.jsx
@@ -1,11 +1,11 @@
 import { useRouteError } from "react-router-dom";
-import styles from "./ErrorPage.module.css";
+import styles from "./OtherPages.module.css";
 
 function ErrorPage() {
     let error = useRouteError();
 
     return (
-        <div className={styles.errorMessage}
+        error && <div className={styles.errorMessage}
         >
             <p>A network error was encountered.</p>
             <p>Please try again later.</p>
