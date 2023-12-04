084a118d166255bed4c6ec23e24989606276b7b7
Amy Jung <ajwjung@outlook.com>
2023-12-04 15:08:42 -0800

    style: Default shop page

--- src/components/ShopPages/DefaultShop.jsx
+++ src/components/ShopPages/DefaultShop.jsx
@@ -1,6 +1,11 @@
+import styles from "./OtherPages.module.css";
+
 function DefaultShop() {
     return (
-        <div>Default Shop</div>
+        <div className={styles.defaultMessage}>
+            <p>Default Shop</p>
+            <p>Nothing to see here.</p>
+        </div>
     )
 }
 
