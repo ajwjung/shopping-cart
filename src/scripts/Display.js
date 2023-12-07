3af158e1a208b7982cb6e22ef5f49a46c048cf57
Amy Jung <ajwjung@outlook.com>
2023-12-07 15:07:30 -0800

    chore: Remove unused images

--- src/scripts/Display.js
+++ src/scripts/Display.js
@@ -1,12 +1,4 @@
 const Display = (() => {
-    const homeImages = [
-        "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1524484485831-a92ffc0de03f?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1623244307563-f9ade3df13c0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1519710164239-da123dc03ef4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"    
-    ]
-
     function capitalizeName(name) {
         return name.split(" ").map(word => {
             return word.charAt(0).toUpperCase() + word.slice(1)
@@ -19,7 +11,7 @@ const Display = (() => {
         return lowercaseDescription.charAt(0).toUpperCase() + lowercaseDescription.slice(1);
     }
 
-    return { homeImages, capitalizeName, formatDescription };
+    return { capitalizeName, formatDescription };
 })();
 
 export default Display;
