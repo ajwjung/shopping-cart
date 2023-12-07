4496d4862468d0d5239627df4f7870315cc99a28
Amy Jung <ajwjung@outlook.com>
2023-12-07 14:20:09 -0800

    style: Change home page UI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,19 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
-import ShopCategory from "./ShopCategory";
-import Display from "../../scripts/Display";
 import styles from "./Home.module.css";
 
 function Home() {
-    const imgLinks = Display.homeImages;
-
     return (
         <>
             <Navbar />
             <div className={styles.homeContent}>
-                <div className={styles.bannerWrapper}>
-                    <img className={styles.banner} src={imgLinks[4]} alt="" />
-                    <p className={`${styles.adText} ${styles.bold}`}>
-                        Shop your dream aesthetic at the right price.
+                <div className={styles.introWrapper}>
+                    <p className={`${styles.intro} ${styles.bold}`}>
+                        Your dream furniture. Our great prices.
                     </p>
-                </div>
-                <div className={styles.categories}>
-                    <ShopCategory
-                        categoryName="All"
-                        image={imgLinks[0]}
-                    />
-                    <ShopCategory
-                        categoryName="Lighting"
-                        image={imgLinks[1]}
-                    />
-                    <ShopCategory
-                        categoryName="Home Decor"
-                        image={imgLinks[2]}
-                    />
-                    <ShopCategory
-                        categoryName="Furniture"
-                        image={imgLinks[3]}
-                    />
+                    <Link className={styles.shopNowBtn} to="/categories/all">
+                        Shop Now
+                    </Link>
                 </div>
             </div>
         </>
